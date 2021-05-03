12c88c8c0a4d2e7b400d2ffe7f0a8cc0d76c9d32
janejane <janetjanedchic@yahoo.com>
2021-05-03 11:11:08 -0400

    ADD: Detail Page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header'
 import Home from './components/Home'
+import Detail from './components/Detail'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-     
+      <Router>
+        <Header />
+        <Switch>
+          
+          <Route path="/detail">
+            <Detail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -11,6 +11,7 @@ function ImgSlider() {
         infinite: true,
         speed: 500,
         slidesToShow: 1,
+        slidesToScroll: 1,
         autoplay: true
     }
 
@@ -22,6 +23,14 @@ function ImgSlider() {
            <Wrap>
                <img src="/images/slider-badag.jpg" alt="" />
            </Wrap>
+           <Wrap>
+               <img src="/images/slider-scale.jpg" alt="" />
+           </Wrap>
+           <Wrap>
+               <img src="/images/slider-scales.jpg" alt="" />
+           </Wrap>
+
+
         </Carousel>
     )
 }
@@ -56,15 +65,14 @@ const Wrap = styled.div`
 cursor: pointer;
 img {
     border: 4px solid transparent;
-    border-radius: 4px;
-    width: 100%;
-    height: 100%;
-    box-shadow: rgb(0 0 0 / 9%) 0px 26px 30px -10px,
-    rgba(0 0 0 / 73%) 0px 16px 10px -10px;
-    transition-duration: 300ms;
-    
-    &:hover {
-        border: 4px solid rgba(249, 249, 249, 0.8);
+        border-radius: 4px;
+        width: 100%;
+        height: 100%;
+        box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+        rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+        transition-duration: 300ms;
+        &:hover {
+            border: 4px solid rgba(249, 249, 249, 0.8);
     }
 }
 
